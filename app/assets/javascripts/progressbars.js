c09196241cead0acda1d70d95ec954fd9514fac5
Ed Jefferson <e.jefferson@gmail.com>
2013-09-20 15:57:15 +0100

    slightly less horrible javascript

--- app/assets/javascripts/progressbars.js
+++ app/assets/javascripts/progressbars.js
@@ -2,62 +2,38 @@ $(document).ajaxComplete(function(){
     var progresstypes=["Calculating average noun sentiment","Deriving cultural reference points","Approximating Myers-Briggs Type","Reticulating splines","Partitioning hashtags","Applying colloquial filters","Assaying content","Dissecting social network pathways","Folding retweets","Segmenting pork statements","Rendering Cleggmania","Squeezing vertexes","Simulating alcohol input","Sequencing regrets"];
 
     $("#result").hide();
+    $('#pr1').hide();
     $('#pr2').hide();
     $('#pr3').hide();
     $('#pr4').hide();
-    i = 1
+    
 
+    function fade(j,time) {
+        setTimeout(function(){
+        $('#pr' + j).fadeIn(250);
+        $('#bar' + j).delay(100).animate({width: "100%"},900);
+        $('#pr' + j).delay(1200).fadeOut();
+    },time * (j-1))
+         
+    }
+    timing = 1800;
+    i = 1;
     while (i < 5)
         {
         index = Math.floor(Math.random() * progresstypes.length);
         $("#text" + i).text(progresstypes.splice(index,1));
-        i = i + 1;
-        }
-    
-   
-    var timing = 1200
+        
+        fade(i,timing)
+        
+        i ++;
 
 
-    $('#bar1').animate({width: "100%"},timing, "swing", function(){
-    
-    $('#pr1').fadeOut(function(){$('#pr2').fadeIn(function(){$('#bar2').animate({width: "100%"},timing, "swing",function(){
-
-
-        $('#pr2').fadeOut(function(){$('#pr3').fadeIn(function(){$('#bar3').animate({width: "100%"},timing, "swing",
-
-            function(){
-
-                $('#pr3').fadeOut(function(){$('#pr4').fadeIn(function(){$('#bar4').animate({width: "100%"},timing, "swing",function(){
-
-
-$('#pr4').fadeOut(function(){$("#result").fadeIn();})
-
-
-                }
+};
+    setTimeout(function(){$("#result").fadeIn();},timing * 4)
 
 
 
 
-                    )})});
-
-            }
-
-
-            )})});
-
-
-
-
-    }
-
-
-        );});});
-    
-    
-
-    
-
-  });
 
   
 
