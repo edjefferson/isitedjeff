4917124f5d29962773ea59b1b31f4858d1c2e17a
Ed Jefferson <e.jefferson@gmail.com>
2013-09-20 15:02:05 +0100

    handle trailing .0s, badly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919154032) do
+ActiveRecord::Schema.define(version: 20130920135216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 20130919154032) do
   create_table "user_checks", force: true do |t|
     t.string   "name"
     t.string   "profile_url"
-    t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "check_count"
+    t.integer  "check_count", default: 0
+    t.decimal  "score"
   end
 
 end
