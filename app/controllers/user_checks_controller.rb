a7a2e658d255e81569f28db1d8be48c152f0d61d
Ed Jefferson <e.jefferson@gmail.com>
2013-09-20 15:21:28 +0100

    downcase

--- app/controllers/user_checks_controller.rb
+++ app/controllers/user_checks_controller.rb
@@ -3,12 +3,12 @@ class UserChecksController < ApplicationController
 
 
   def search
-    name_to_check = params[:q].downcase || ""
+    name_to_check = params[:q] || ""
     if name_to_check[0] == "@"
       name_to_check = name_to_check[1..-1]
     end
 
-    @user_check = UserCheck.where(name: name_to_check).first_or_create
+    @user_check = UserCheck.where(name: name_to_check.downcase).first_or_create
     if @user_check.score == nil
       @user_check.score = ((0..15).to_a.sample.to_s + "." + (0..9).to_a.sample.to_s).to_f
       @user_check.save
