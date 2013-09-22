b43939837d168364c8b620aa0017927e659f9849
Ed Jefferson <e.jefferson@gmail.com>
2013-09-22 14:53:07 +0100

    make list sortable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  
 end
--- app/controllers/user_checks_controller.rb
+++ app/controllers/user_checks_controller.rb
@@ -1,33 +1,52 @@
 class UserChecksController < ApplicationController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @user_checks = UserCheck.all
+    @user_checks = UserCheck.order(sort_column + " " + sort_direction)
   end
+
+
 
   def robots                                                                                                                                      
     robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
     render :text => robots, :layout => false, :content_type => "text/plain"
   end
-  
+
   def search
-    name_to_check = params[:q] || ""
-    if name_to_check[0] == "@"
-      name_to_check = name_to_check[1..-1]
+    if params[:q]
+      name_to_check = params[:q]
+      if name_to_check[0] == "@"
+        name_to_check = name_to_check[1..-1]
+      end
+
+      @user_check = UserCheck.where(name: name_to_check.downcase).first_or_create
+      if @user_check.score == nil
+        @user_check.score = ((0..15).to_a.sample.to_s + "." + (0..9).to_a.sample.to_s).to_f
+        
+      end
+      @user_check.check_count += 1
+      @user_check.save
+
+      respond_to do |format|
+        format.html
+        format.js {}
+      end
+    else 
+      @user_check = UserCheck.new
+      @user_check.name = ""
     end
 
-    @user_check = UserCheck.where(name: name_to_check.downcase).first_or_create
-    if @user_check.score == nil
-      @user_check.score = ((0..15).to_a.sample.to_s + "." + (0..9).to_a.sample.to_s).to_f
-      
-    end
-    @user_check.check_count += 1
-    @user_check.save
+  end
 
-    respond_to do |format|
-      format.html
-      format.js {}
-    end
+  private
 
+  def sort_column
+    UserCheck.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
